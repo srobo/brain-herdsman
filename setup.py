546d271494a2b3cf420a78bac0e8c8de5eb7c5c1
Thomas Leese <inbox@thomasleese.me>
2015-10-05 12:02:41 +0100

    Improve code formatting of 'setup.py'
    Also make version dependencies a little more strict.

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
 import ez_setup
 ez_setup.use_setuptools()
+
 from setuptools import setup, find_packages
 
+
 setup(
-    name = "herdsman",
-    version = "0.1",
-    packages = find_packages(),
-    scripts = ["bin/herdsman"],
-    description = "Server for herding user's code running on the Student Robotics platform",
-    install_requires = ["autobahn >= 0.8.10",
-                        "flask >= 0.10",
-                        "pyyaml >= 3",
-                        "zope.interface >= 4.0.5",
-                        "Twisted >= 12.2.0",
-                        "sr.robot >= 0.1",
-                    ],
+    name='herdsman',
+    version='0.1.0',
+    packages=find_packages(),
+    scripts=['bin/herdsman'],
+    description="Server for herding user's code running on the Student "
+                "Robotics platform",
+    install_requires=[
+        'autobahn >=0.10, <1',
+        'Flask >=0.10, <1',
+        'PyYAML >=3.11, <4',
+        'zope.interface >=4.1, <5',
+        'Twisted >=15.4, <16',
+        'sr.robot >= 0.1, <1',
+    ],
 )
