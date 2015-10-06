ca0371e269c92986c488ca4aa98a5d8796d650ab
Thomas Leese <inbox@thomasleese.me>
2015-10-06 16:10:06 +0100

    Add some example user codes

---
+++ example-usercodes/custom-pages-with-flask/robot.py
@@ -0,0 +1,39 @@
+from __future__ import print_function
+
+import flask
+
+from sr.robot import Robot
+
+
+R = Robot()
+app = flask.Flask(__name__)
+
+
+@app.route('/pages')
+def pages():
+    return flask.jsonify(pages=[
+        {
+            'name': 'my-custom-page',
+            'title': 'My Custom Page',
+            'import_url': '/pages/my-page'
+        }
+    ]), 200, {'Access-Control-Allow-Origin': '*'}
+
+
+@app.route('/pages/my-page')
+def my_page():
+    return """
+        <dom-module name="my-custom-page">
+            <template>
+                <h1>It's a custom page!</h1>
+            </template>
+
+            <script>
+                Polymer({
+                    is: 'my-custom-page'
+                });
+            </script>
+        </dom-module>
+    """, 200, {'Access-Control-Allow-Origin': '*'}
+
+app.run(port=10000)
